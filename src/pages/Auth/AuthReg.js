b92151545e00d78aedc2b3b60dc7bcb590c74cf7
Siddharth0101 <siddsikarwar2001@gmail.com>
2024-06-10 21:46:45 +0530

    registering

--- src/pages/Auth/AuthReg.js
+++ src/pages/Auth/AuthReg.js
@@ -1,7 +1,33 @@
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 import Form from "react-bootstrap/Form";
+import { useRef } from "react";
 const AuthReg = (props) => {
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    const email = emailRef.current.value;
+    const password = passwordRef.current.value;
+    try {
+      await fetch(
+        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDLKz4YyiCc2HS-nLD1Czl8a4iO8wXs92M",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            email: email,
+            password: password,
+            returnSecureToken: true,
+          }),
+        }
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <div>
       <Modal
@@ -18,22 +44,23 @@ const AuthReg = (props) => {
           </div>
         </Modal.Header>
         <Modal.Body>
-          <Form>
+          <Form onSubmit={submitHandler}>
             <Form.Group className="mb-3" controlId="formBasicEmail">
               <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" />
-              <Form.Text className="text-muted">
-                We'll never share your email with anyone else.
-              </Form.Text>
+              <Form.Control
+                type="email"
+                placeholder="Enter email"
+                ref={emailRef}
+              />
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formBasicPassword">
               <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-              <Form.Label>Confirm Password</Form.Label>
-              <Form.Control type="password" placeholder="Confirm Password" />
+              <Form.Control
+                type="password"
+                placeholder="Password"
+                ref={passwordRef}
+              />
             </Form.Group>
             <Button variant="primary" type="submit">
               Submit
