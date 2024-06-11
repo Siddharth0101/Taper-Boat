b142feebda42cbb2d9baec4faa5ba6fadb95994c
Siddharth0101 <siddsikarwar2001@gmail.com>
2024-06-11 12:24:01 +0530

    update Auth

--- src/pages/Auth/Auth.js
+++ src/pages/Auth/Auth.js
@@ -1,7 +1,37 @@
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 import Form from "react-bootstrap/Form";
+import { useRef } from "react";
 const Auth = (props) => {
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    const email = emailRef.current.value;
+    const password = passwordRef.current.value;
+    try {
+      const response = await fetch(
+        "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDLKz4YyiCc2HS-nLD1Czl8a4iO8wXs92M",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            email: email,
+            password: password,
+            returnSecureToken: true,
+          }),
+        }
+      );
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <div>
       <Modal
@@ -16,10 +46,14 @@ const Auth = (props) => {
           </div>
         </Modal.Header>
         <Modal.Body>
-          <Form>
+          <Form onSubmit={submitHandler}>
             <Form.Group className="mb-3" controlId="formBasicEmail">
               <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" />
+              <Form.Control
+                type="email"
+                placeholder="Enter email"
+                ref={emailRef}
+              />
               <Form.Text className="text-muted">
                 We'll never share your email with anyone else.
               </Form.Text>
@@ -27,7 +61,11 @@ const Auth = (props) => {
 
             <Form.Group className="mb-3" controlId="formBasicPassword">
               <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" />
+              <Form.Control
+                type="password"
+                placeholder="Password"
+                ref={passwordRef}
+              />
               <Form.Text className="text-muted">
                 We'll never share your password with anyone else.
               </Form.Text>
